a60f19ee0700e55f1299947d42cee37804fbd21d
Deibson Cogo <deibsoncogo@outlook.com>
2024-02-05 11:45:21 -0300

    ✨ feat: criação do component side menu

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,17 +1,32 @@
+"use client"
+
 import Image from "next/image"
+import Link from "next/link"
 import { MenuIcon } from "lucide-react"
+import SideMenu from "./side-menu"
 import { Button } from "./ui/button"
 import { Card, CardContent } from "./ui/card"
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
 
 const Header = () => {
   return (
     <Card>
       <CardContent className="flex flex-row items-center justify-between p-5">
-        <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+        <Link href="/">
+          <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+        </Link>
 
-        <Button variant="outline" size="icon">
-          <MenuIcon size={16} />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <MenuIcon size={16} />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent className="p-0">
+            <SideMenu />
+          </SheetContent>
+        </Sheet>
       </CardContent>
     </Card>
   )
